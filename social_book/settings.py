73eda6bb34e41a179c33c0a98ad284b9a16dbc15
Darkil259 <Darkil@yandex.ru>
2024-06-29 21:27:27 +0300

    hot fix

--- social_book/settings.py
+++ social_book/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 if DEBUG:
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
